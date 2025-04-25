356be3bff877846f6e528bd3e22c7ef8602386da
Dashank Doshi <72262399+10doshi12@users.noreply.github.com>
2025-04-26 03:11:24 +0530

    Initial commit

---
+++ src/Card.jsx
@@ -0,0 +1,50 @@
+export default () => (
+  <div className="birthdayCard">
+    <div className="cardFront">
+      <h2 className="happy">HAPPY BIRTHDAY SAKSHI MAM</h2>
+      <div className="balloons">
+        <div className="balloonOne" />
+        <div className="balloonTwo" />
+        <div className="balloonThree" />
+        <div className="balloonFour" />
+      </div>
+    </div>
+    <div className="cardInside">
+      <a href="https://voraclasses.com">
+        <h3 className="back">HAPPY BIRTHDAY TO THE BEST CHEMISTRY TEACHER</h3>
+      </a>
+      <p>Hello Mam,</p>
+      <p>
+        Happy birthday!! I hope your day is filled with lots of love and
+        laughter! You are one of the beat teachers and mentors I have come
+        across in My life. In this short span of two months and two years online
+        you have taught me so much and I am very grateful and honored to have
+        you as my teacher. I truly Believe the you will reach new heights, break
+        boundaries and help more students like me to fall in love with chemistry
+        as an Interesting subject and not just part of JEE Syllabus.
+      </p>
+      <p>from Dashank</p>
+      <a href="https://en.wikipedia.org/wiki/Benzene">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Benzene_200.svg/1200px-Benzene_200.svg.png"
+          alt=""
+          class="benzene"
+        />
+      </a>
+      <a href="https://en.wikipedia.org/wiki/Picric_acid">
+        <img
+          src="https://www.vedantu.com/question-sets/df8b4ed8-7eb5-4c2f-ba70-2158009c78c09141014807045403802.png"
+          alt=""
+          class="picricacid"
+        />
+      </a>
+      <a href="https://en.wikipedia.org/wiki/Fullerene">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/1/16/Fullerene.png"
+          alt=""
+          class="fullerene"
+        />
+      </a>
+    </div>
+  </div>
+);
